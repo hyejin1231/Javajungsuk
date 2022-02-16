f80e91d51c77352e0cbf9754b75a8294b415aa07
hyejin1231 <phj981231@naver.com>
2022-02-16 10:24:01 +0900

    20220216 ch5. 배열 Array

---
+++ src/main/java/hello/code/Ex5_2.java
@@ -0,0 +1,36 @@
+package hello.code;
+
+import java.util.Arrays;
+
+public class Ex5_2 {
+
+    public static void main(String[] args) {
+        int sum = 0 ;
+        float average = 0f;
+
+        int[] score = {100, 40, 50, 60, 90};
+
+        int max = score[0];
+        int min = score[0];
+
+        for (int i : score) {
+            sum += i;
+        }
+
+        average = sum / (float) score.length;
+
+        for (int i : score) {
+            if (max < i) {
+                max = i;
+            } else if (min > i) {
+                min = i;
+            }
+        }
+
+        System.out.println("총합 = " + sum);
+        System.out.println("평균 = " + average);
+
+        System.out.println("최소값 = " + min);
+        System.out.println("최대값 = " + max);
+    }
+}
